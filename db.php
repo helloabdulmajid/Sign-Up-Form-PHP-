0d9457b66d7e554133fe12cc7204c86ae103af4e
Majid <helloabdulmajid>
2022-02-14 18:40:33 +0530

    First Init & Working Fine

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+
+	$database = "formsubmit";
+
+	// Create a connection
+	$conn = mysqli_connect($servername,
+		$username, $password, $database);
+
+	
+
+?>
